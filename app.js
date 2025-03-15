354ac23320c40d410edab88e9bc6f3d2217547ff
Alex Alanis <ale.x.israel@hotmail.com>
2025-03-15 14:01:48 -0600

    Se impleento la funcion para agregar amigos y lo limitamos a 10 registros

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 
 let listaAmigos = [];       //lista para almacenar nombres
 
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombre = inputAmigo.value.trim();             //Registro del nombre
+
+    if (nombre === "") {                                    //Verifica que exista un nombre
+        alert ("Por favor, inserte un nombre");
+        return;
+    } else if (listaAmigos.length === 10) {                   //Valida un maximo de registros
+        alert ("Alcanzaste el número máximo de registros");
+        return;
+    } else {
+        listaAmigos.push(nombre); //Añadimos el nombre a la lista
+        inputAmigo.value = "";        
+    }
+}
+
