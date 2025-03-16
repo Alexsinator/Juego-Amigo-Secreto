dc5602380ed44ff9d68d29753982ab4f8d323ee0
Alex Alanis <ale.x.israel@hotmail.com>
2025-03-15 20:46:23 -0600

    Mejora en app.js, en la funcion sortearAmigo se manda un mensaje adicional para que se tengan que ingresar minimo dos nombres, y se establece támbien el numero maximo en la parte superior del codigo, para cuando se requiera cambiar la cantidad maxima de registros

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaAmigos = [];       //lista para almacenar nombres
+let numeroMaximoRegistros = 10;
+
 
 function agregarAmigo() {
     const inputAmigo = document.getElementById("amigo");
@@ -9,7 +11,7 @@ function agregarAmigo() {
     if (nombre === "") {                                    //Verifica que exista un nombre
         alert ("Por favor, inserte un nombre");
         return;
-    } else if (listaAmigos.length === 10) {                   //Valida un maximo de registros
+    } else if (listaAmigos.length === numeroMaximoRegistros) {                   //Valida un maximo de registros
         alert ("Alcanzaste el número máximo de registros");
         return;
     } else {
@@ -33,6 +35,9 @@ function actualizarLista() {
 function sortearAmigo() {
     if (listaAmigos.length === 0) {
         alert ("Por favor, ingresa al menos 2 nombres");
+        return;
+    }   if (listaAmigos.length < 2){
+        alert ("Es necesario ingresar 2 nombres para realizar el sorteo.");
         return;
     }
             const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
